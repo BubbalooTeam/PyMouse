7ec30b87d3f88d8c774a7e5899c3b3f6618d2ef9
DaviisDev <00001122977141sp@al.educacao.sp.gov.br>
2024-05-12 17:51:02 -0300

    Feat(handler): Handling `speed_test` function

--- pymouse/services/load_handler/run/__init__.py
+++ pymouse/services/load_handler/run/__init__.py
@@ -9,5 +9,6 @@ class RunModules:
         load_modules.add_cmd(sudoers.Sudoers_Plugins().ping, "ping")
         load_modules.add_cmd(sudoers.Sudoers_Plugins().rr, ["rr", "restart"])
         load_modules.add_cmd(sudoers.Sudoers_Plugins().shutdown, "shutdown")
+        load_modules.add_cmd(sudoers.Sudoers_Plugins().speed_test, "speedtest")
         # Final loader => Leave "load_modules.cmds_loader()" last, if you put it before the commands will not be added correctly, causing failures!
         load_modules.cmds_loader()
