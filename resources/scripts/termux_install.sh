719a7e12536ae660cf3f8495fe01f14c881ffef5
DaviisDev <00001122977141sp@al.educacao.sp.gov.br>
2024-05-15 19:21:12 -0300

    Fix

--- resources/scripts/termux_install.sh
+++ resources/scripts/termux_install.sh
@@ -3,13 +3,12 @@ pkg update -y
 apt update
 apt update -y
 
-python_not_installed="$(python -c 'exit()')"
 # Check if python is installed
-if [ python_not_installed ]
+if ! command -v python &> /dev/nul
 then
-    printf "Python3 not Found, Installing Python. . .\n"
+    printf "Python not Found, Installing Python. . .\n"
     pkg install python python-pip -y
 fi
 
-printf "Ok!! Updates of Termux is terminated! Go to installation of PyMouse. . .\n\n\n"
+printf "Ok!! Updates of Termux is terminated! Proceed to the installation of PyMouse. . .\n\n\n"
 python resources/termux/__init__.py
