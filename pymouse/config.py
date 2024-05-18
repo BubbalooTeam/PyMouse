285ecd52917adaff58b6932f689d17d253241e28
DaviisDev <00001122977141sp@al.educacao.sp.gov.br>
2024-05-18 18:09:03 -0300

    @BubbalooTeam

--- pymouse/config.py
+++ pymouse/config.py
@@ -4,22 +4,29 @@ from pymouse import log
 from dotenv import load_dotenv
 
 # === #
+vars = {
+    "API_ID": "Required",
+    "API_HASH": "Required",
+    "BOT_TOKEN": "Required",
+    "WORKERS": "Optional",
+    "LOG_CHANNEL": "Required",
+    "TRIGGER": "Optional",
+    "IPV6": "Optional",
+    "OWNER_ID": "Required"
+}
+
 class MakeConfig:
     def make_config(self):
         with open("config.env", "a") as file:
-            for var in [
-                "API_ID", 
-                "API_HASH", 
-                "BOT_TOKEN", 
-                "WORKERS", 
-                "LOG_CHANNEL", 
-                "TRIGGER",
-                "IPV6",
-                "OWNER_ID",
-            ]:
+            for var in vars.keys():
                 inp = input("Enter {variable}\n: ".format(variable=var))
-                file.write('{variable}="{inputed}"\n'.format(variable=var, inputed=inp))
-        log.info("Sucessfully created config.env file...")
+                if inp == "" and vars.get(var, "Optional") != "Required":
+                    continue
+                else:
+                    while inp == "":
+                        inp = input("Please enter a value for {variable}\n: ".format(variable=var))
+                    file.write('{variable}="{inputed}"\n'.format(variable=var, inputed=inp))
+        log.info("Successfully created config.env file...")
 # === #
 
 
@@ -31,12 +38,12 @@ else:
 
 
 class Config:
-    API_ID = int(os.getenv("API_ID", 6))
+    API_ID = int(os.getenv("API_ID"))
     API_HASH = os.getenv("API_HASH")
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     WORKERS = int(os.getenv("WORKERS", 24))
     LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
     TRIGGER = os.getenv("TRIGGER", "/ !".split())
     IPV6 = bool(os.getenv("IPV6", False))
-    OWNER_ID = int(os.getenv("OWNER_ID", 1715384854))
+    OWNER_ID = int(os.getenv("OWNER_ID"))
     VERSION = "1.0.0"
