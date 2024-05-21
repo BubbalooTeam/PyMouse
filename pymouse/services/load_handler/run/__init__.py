59c6033128b370b4833c8f3a865c9e4a5b3ad17b
DaviisDev <00001122977141sp@al.educacao.sp.gov.br>
2024-05-21 19:02:11 -0300

    feat(Handler): Adding regex_loader()

--- pymouse/services/load_handler/run/__init__.py
+++ pymouse/services/load_handler/run/__init__.py
@@ -8,10 +8,12 @@ class RunModules:
         )
         # Sudoers
         load_modules.add_cmd(afk.AFK_Plugins().setupAFK, "afk", "AFK", "Mark yourself away (away from the keyboard).")
-        load_modules.add_regex(afk.AFK_Plugins().setupAFK, r"^(?i)brb(\s(?P<args>.+))?")
+        load_modules.add_regex(afk.AFK_Plugins().setupAFK, r"^(?i)brb(\s(.+))?")
         load_modules.add_cmd(sudoers.Sudoers_Plugins().ping, "ping")
         load_modules.add_cmd(sudoers.Sudoers_Plugins().rr, ["rr", "restart"])
         load_modules.add_cmd(sudoers.Sudoers_Plugins().shutdown, "shutdown")
         load_modules.add_cmd(sudoers.Sudoers_Plugins().speed_test, "speedtest")
         # Final loader => Leave "load_modules.cmds_loader()" last, if you put it before the commands will not be added correctly, causing failures!
         load_modules.cmds_loader()
+        # Final loader => Leave "load_modules.regex_loader()" last, if you put it before the regex will not be added correctly, causing failures!
+        load_modules.regex_loader()
