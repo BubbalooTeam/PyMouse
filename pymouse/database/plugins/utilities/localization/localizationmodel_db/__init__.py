d673ce7bd4348fc9ffbb00fdad4b790d56c5866a
DaviisDev <00001122977141sp@al.educacao.sp.gov.br>
2024-05-26 17:24:54 -0300

    Feat(database/localization): Added `get_chat_language` function.

--- pymouse/database/plugins/utilities/localization/localizationmodel_db/__init__.py
+++ pymouse/database/plugins/utilities/localization/localizationmodel_db/__init__.py
@@ -5,14 +5,36 @@ from hydrogram.enums import ChatType
 from pymouse import db, log
 
 class LocalizationDB:
-    def get_database_context(
-        self,
-        union: Union[Message, CallbackQuery, InlineQuery]
+    @staticmethod
+    def get_chatid(
+        union: Union[Message, CallbackQuery, InlineQuery],
     ):
         """
-        Returns the appropriate database collection based on the message or query type.
+        Returns the chatID.
 
-        Args:
+        Arguments:
+            union (Union[Message, CallbackQuery, InlineQuery]): The received message or query.
+
+        Returns:
+            int | None: The chat ID or None if not supported.
+        """
+        if isinstance(union, InlineQuery):
+            return union.from_user.id
+        elif isinstance(union, Union[Message, CallbackQuery]):
+            msg = union.message if isinstance(union, CallbackQuery) else union
+            return msg.chat.id
+        else:
+            log.critical("This instance is not supported!")
+            return None
+
+    @staticmethod
+    def get_database_context(
+        union: Union[Message, CallbackQuery, InlineQuery],
+    ):
+        """
+        Returns the appropriate database collection.
+
+        Arguments:
             union (Union[Message, CallbackQuery, InlineQuery]): The received message or query.
 
         Returns:
@@ -30,6 +52,32 @@ class LocalizationDB:
             else:
                 log.critical("Chat type with enum (%s) is not supported!", chat_type)
                 return None
+        else:
+            log.critical("This instance is not supported!")
+            return None
+            
+    def get_chat_language(
+        self,
+        union: Union[Message, CallbackQuery, InlineQuery],
+    ):
+        """
+        Returns the chat language.
 
+        Arguments:
+            union (Union[Message, CallbackQuery, InlineQuery]): The received message or query.
+
+        Returns:
+            str | None: The chat language or None if not supported.
+        """
+        db_ctx = self.get_database_context(union)
+        chat_id = self.get_chatid(union)
+        tck_type = "chat_id" if db_ctx.collection == "chats" else "user_id"
+        get_chat = db_ctx.find_one({tck_type: chat_id})
+        if get_chat is not None:
+            language_class = get_chat.get("localization", {})
+            language = language_class.get("chat_lang", None)
+            return language
+        else:
+            return None
 
 localization_db = LocalizationDB()
