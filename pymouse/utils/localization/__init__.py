11f70368bae4658b4602f254befc818342640bd0
DaviisDev <00001122977141sp@al.educacao.sp.gov.br>
2024-05-28 21:03:35 -0300

    Feat(localization): Added function `get_statistics` for get Language informations.

--- pymouse/utils/localization/__init__.py
+++ pymouse/utils/localization/__init__.py
@@ -2,11 +2,46 @@ import json
 import os
 from pathlib import Path
 from typing import Union, List
+from dataclasses import dataclass
 
 from hydrogram.types import Message, CallbackQuery, InlineQuery
 
 from ..logger import log
 from ...database.plugins.utilities.localization import localizationmodel_db
+
+@dataclass(frozen=True, slots=True)
+class LocalizationStats:
+    """
+    Represents a Localization Statistics:
+        * Get informations with `number/percentage of strings translated/untranslated`
+    """
+    total_strings: int
+    """
+    The total number of strings.
+
+    `type:` int
+    """
+
+    translated: int
+    """
+    The number of translated strings.
+
+    `type:` int
+    """
+
+    untranslated: int
+    """
+    The number of untranslated strings.
+
+    `type:` int
+    """
+
+    percentage_translated: int
+    """
+    The percentage of translated strings.
+
+    `type:` int
+    """
 
 class Localization:
     def __init__(self):
@@ -35,10 +70,49 @@ class Localization:
         log.info("Localization compiled successfully!")
 
     def get_localization_of_chat(self, union: Union[Message, CallbackQuery, InlineQuery]):
+        """Get chat Localization."""
         locale = localizationmodel_db.localization_db.get_chat_language(union)
         if locale is None:
             return self.default_language
         else:
             return locale if locale in self.current_locales else self.default_language
+        
+    def get_statistics(self, language: str):
+        default_language = self.strings.get(self.default_language, {})
+        requested_language = self.strings.get(language, {})
+        def recursive_count_strings(default_value, requested_value):
+            if isinstance(requested_value, str):
+                nonlocal total_strings, translated_strings
+                total_strings += 1
+                if requested_value != default_value:
+                    translated_strings += 1
+            elif isinstance(requested_value, dict):
+                for subkey in requested_value:
+                    if subkey in default_value:
+                        recursive_count_strings(default_value[subkey], requested_value[subkey])
+
+        total_strings = 0
+        translated_strings = 0
+
+        for key in default_language:
+            if key in requested_language:
+                default_value = default_language[key]
+                requested_value = requested_language[key]
+                recursive_count_strings(default_value, requested_value)
+
+        if total_strings > 0:
+            translation_percentage = (translated_strings / total_strings) * 100
+        else:
+            translation_percentage = 0
+
+        untranslated_strings = total_strings - translated_strings
+
+        return LocalizationStats(
+            total_strings=total_strings, 
+            translated=translated_strings, 
+            untranslated=untranslated_strings, 
+            percentage_translated=translation_percentage
+        )
+
 
 localization = Localization()
