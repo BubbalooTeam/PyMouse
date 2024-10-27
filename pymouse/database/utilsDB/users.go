facb22ecfece7f8b1e7de70512ba6ddf014d4be5
DaviisDev <00001122977141sp@al.educacao.sp.gov.br>
2024-10-27 18:28:42 -0300

    Some improviments...

--- pymouse/database/utilsDB/users.go
+++ pymouse/database/utilsDB/users.go
@@ -3,21 +3,15 @@ package utilsdb
 import (
 	"log"
 	"pymouse/pymouse/database"
+	"pymouse/pymouse/database/typesdb"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type UsersInformations struct {
-	UserID    int64  `bson:"user_id,omitempty" json:"user_id,omitempty"`
-	UserName  string `bson:"username" json:"username" default:"nil"`
-	FirstName string `bson:"first_name" json:"first_name" default:"nil"`
-	Language  string `bson:"language" json:"language" default:"en_us"`
-}
-
-func FindUser(UserID int64) (UI *UsersInformations) {
+func FindUser(UserID int64) (UI *typesdb.UsersInformations) {
 	UsersCollection := database.NewMongoCollection("users")
-	dftUser := &UsersInformations{
+	dftUser := &typesdb.UsersInformations{
 		UserID: UserID,
 	}
 	err := UsersCollection.FindOne(bson.M{"user_id": UserID}).Decode(&UI)
@@ -41,7 +35,7 @@ func UpdateUser(UserID int64, UserName string, FirstName string) {
 		UI.FirstName = FirstName
 		UI.UserName = UserName
 	} else {
-		UI = &UsersInformations{
+		UI = &typesdb.UsersInformations{
 			UserID:    UserID,
 			UserName:  UserName,
 			FirstName: FirstName,
