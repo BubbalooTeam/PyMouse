1ad2da0181276d68908f6ded83a8727084a86977
DaviisDev <00001122977141sp@al.educacao.sp.gov.br>
2024-10-28 11:44:10 -0300

    hmm..

--- pymouse/modules/checkers/handlers.go
+++ pymouse/modules/checkers/handlers.go
@@ -2,7 +2,7 @@ package checkers
 
 import (
 	"fmt"
-	utilsDB "pymouse/pymouse/database/utilsdb"
+	"pymouse/pymouse/database/utilsdb"
 
 	"github.com/mymmrac/telego"
 	th "github.com/mymmrac/telego/telegohandler"
@@ -29,7 +29,7 @@ func SaveUsers(bot *telego.Bot, update telego.Update, next th.Handler) {
 	}
 
 	// Update or Insert User Informations
-	utilsDB.UpdateUser(UserID, UserName, FirstName)
+	utilsdb.UpdateUser(UserID, UserName, FirstName)
 
 	// Pass to next handler
 	next(bot, update)
