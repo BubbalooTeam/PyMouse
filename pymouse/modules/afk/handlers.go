9eefef4502430d54dd1c89dbfacfd15b01a14bd8
DaviisDev <00001122977141sp@al.educacao.sp.gov.br>
2024-11-02 09:37:28 -0300

    Ready, improviments on Away.

--- pymouse/modules/afk/handlers.go
+++ pymouse/modules/afk/handlers.go
@@ -66,12 +66,14 @@ func CheckAway(bot *telego.Bot, update telego.Update, next th.Handler) {
 	message := update.Message
 	re := regexp.MustCompile(`(?i)^\b(afk|brb)\b`)
 
-	if message == nil || message.From == nil || !strings.Contains(message.Chat.Type, "group") || re.MatchString(message.Text) {
+	if message == nil ||
+		message.From == nil ||
+		(strings.Contains(message.Chat.Type, "private") && re.MatchString(message.Text)) {
 		next(bot, update)
 		return
 	}
 
-	if message.From != nil && utilitiesdb.GetAway(message.From.ID).IsAway {
+	if utilitiesdb.GetAway(message.From.ID).IsAway {
 		StopAway(bot, update)
 		next(bot, update)
 		return
