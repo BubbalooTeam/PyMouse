5bb5491f5c8a48e800fadc78bb35dc636160df2a
DaviisDev <00001122977141sp@al.educacao.sp.gov.br>
2024-11-05 18:42:04 -0300

    ++

--- pymouse/helpers/keyboard/pagination.go
+++ pymouse/helpers/keyboard/pagination.go
@@ -31,7 +31,7 @@ func AddButton(Text string, CallbackPattern string) telego.InlineKeyboardButton 
 	}
 }
 
-func BuildPagination(TotalPages int, CurrentPage int, CallbackPattern string) *telego.InlineKeyboardMarkup {
+func Paginate(TotalPages int, CurrentPage int, CallbackPattern string) *telego.InlineKeyboardMarkup {
 	var IKB [][]telego.InlineKeyboardButton
 	if TotalPages <= 5 {
 		IKB = append(IKB, FullPagination(TotalPages, CurrentPage, CallbackPattern)...)
