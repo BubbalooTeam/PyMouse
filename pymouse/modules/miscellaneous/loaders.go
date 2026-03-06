caac6bbd24a4bef5126e4bd95e41089dfacebcdb
DaviDev <97841570+DaviisDev@users.noreply.github.com>
2026-03-06 01:36:07 +0000

    feat(env): add example environment configuration file

--- pymouse/modules/miscellaneous/loaders.go
+++ pymouse/modules/miscellaneous/loaders.go
@@ -13,8 +13,10 @@ import (
 
 func LoadModule(bS *client.BotStruct) {
 	middlewares.Help.RegisterHelp("Miscellaneous", middlewares.Help.WithSubmodules("GSMArena", "Weather"))
+	// GSMArena Handlers
 	bS.Handler.Handle(gsmarena.DeviceSearch, telegram.Command("d"))
 	bS.Handler.Handle(gsmarena.DeviceSearchPagination, th.CallbackDataMatches(regexp.MustCompile(`^gsm_page\|(.*)$`)))
 	bS.Handler.Handle(gsmarena.DeviceSearchSelect, th.CallbackDataMatches(regexp.MustCompile(`^d\|(.*)$`)))
+	// Weather Handlers
 	bS.Handler.Handle(weather.WeatherHandler, telegram.Command("weather"))
 }
