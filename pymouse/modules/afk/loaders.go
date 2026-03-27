9ba77f9e4ceed43228610ecc86f2635072e16693
DaviDev <97841570+DaviisDev@users.noreply.github.com>
2026-03-27 11:47:21 +0000

    + +

--- pymouse/modules/afk/loaders.go
+++ pymouse/modules/afk/loaders.go
@@ -4,10 +4,17 @@ import (
 	"pymouse/pymouse/client"
 	"pymouse/pymouse/helpers/telegram"
 	"pymouse/pymouse/middlewares"
+	"regexp"
+
+	"github.com/mymmrac/telego/telegohandler"
 )
 
 func LoadModule(bS *client.BotStruct) {
 	middlewares.Help.RegisterHelp("AFK")
-	bS.Handler.Handle(SetAway, telegram.Command("afk"))
+	bS.Handler.Handle(SetAway, telegohandler.Or(
+		telegram.Command("afk"),
+		telegohandler.TextMatches(regexp.MustCompile(`^(?:brb)(\s.+)?`)),
+	),
+	)
 	bS.Handler.Use(CheckAway)
 }
