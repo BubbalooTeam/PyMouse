06183ef74570cae14c21cafa3a04e2eccec897ca
DaviDev <97841570+DaviisDev@users.noreply.github.com>
2026-05-12 00:18:08 +0000

    ++

--- pymouse/modules/lastfm/loaders.go
+++ pymouse/modules/lastfm/loaders.go
@@ -2,13 +2,18 @@ package lastfm
 
 import (
 	"pymouse/pymouse/client"
+	"pymouse/pymouse/config"
 	"pymouse/pymouse/helpers/telegram"
+	"pymouse/pymouse/middlewares"
 	"pymouse/pymouse/modules/lastfm/lfm"
 	"pymouse/pymouse/modules/lastfm/set"
 )
 
 func LoadModule(bS *client.BotStruct) {
-	bS.Handler.Handle(lfm.NowPlaying, telegram.Command("lfm", bS.Client.Username()))
-	bS.Handler.Handle(set.SetUser, telegram.Command("set", bS.Client.Username()))
-	bS.Handler.Handle(set.UnsetUser, telegram.Command("unset", bS.Client.Username()))
+	if config.LastFMAPIKey != "" {
+		middlewares.Help.RegisterHelp("LastFM")
+		bS.Handler.Handle(lfm.NowPlaying, telegram.Command("lfm", bS.Client.Username()))
+		bS.Handler.Handle(set.SetUser, telegram.Command("set", bS.Client.Username()))
+		bS.Handler.Handle(set.UnsetUser, telegram.Command("unset", bS.Client.Username()))
+	}
 }
