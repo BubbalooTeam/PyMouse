5791c758ca17cc8eff6e3ed89458bc95bc868f50
DaviDev <97841570+DaviisDev@users.noreply.github.com>
2026-05-17 23:31:09 +0000

    feat(lastfm): improve LastFM command handling and update usage instructions

--- pymouse/modules/lastfm/loaders.go
+++ pymouse/modules/lastfm/loaders.go
@@ -7,12 +7,21 @@ import (
 	"pymouse/pymouse/middlewares"
 	"pymouse/pymouse/modules/lastfm/lfm"
 	"pymouse/pymouse/modules/lastfm/set"
+	"regexp"
+
+	"github.com/mymmrac/telego/telegohandler"
 )
 
 func LoadModule(bS *client.BotStruct) {
 	if config.LastFMAPIKey != "" {
 		middlewares.Help.RegisterHelp("LastFM")
-		bS.Handler.Handle(lfm.NowPlaying, telegram.Command("lfm", bS.Client.Username()))
+		bS.Handler.Handle(lfm.NowPlaying,
+			telegohandler.Or(
+				telegram.Command("lfm", bS.Client.Username()),
+				telegram.Command("lt", bS.Client.Username()),
+				telegram.Command("lastfm", bS.Client.Username()),
+				telegohandler.TextMatches(regexp.MustCompile(`^(?:lt)`)),
+			))
 		bS.Handler.Handle(set.SetUser, telegram.Command("set", bS.Client.Username()))
 		bS.Handler.Handle(set.UnsetUser, telegram.Command("unset", bS.Client.Username()))
 	}
